dc4eb44bcd1e0fda4c06fb6b886e9fbb9d214c07
shivamk001 <shivamkesarwani001@gmail.com>
2025-03-12 14:39:08 +0530

    navbar, spotlight, carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { NavbarDemo } from "@/components/Navbar";
-
+import { SpotlightPreview } from "@/components/Spotlight";
+import { CarouselDemo } from "@/components/Carousel";
 
 export default function Home() {
   return (<>
+    <SpotlightPreview/>
     <NavbarDemo/>
+    <CarouselDemo/>
   </>);
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
     <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 border border-indigo-600 rounded-full", className)}
     >
       <Menu setActive={setActive}>
         <MenuItem setActive={setActive} active={active} item="Services">
