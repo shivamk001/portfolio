634989d54dc3d88f9fc2720733c2967faf1b794c
shivamk001 <shivam.kesarwani001@gmail.com>
2025-04-10 23:03:47 +0530

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Hero from "@/components/Hero";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex 
+      justify-center items-center flex-col 
+      overflow-hidden mx-auto sm:px-10">
+      <div className="max-w-7xl w-full">
+        <Hero/>
+      </div>
+      <div>
+        <h1>Hello, Portfolio</h1>
+      </div>
+    </main>
+  );
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-    remotePatterns: [
-    {
-        protocol: "https",
-        hostname: "assets.aceternity.com",
-    },
-    ],
-},
-};
+const nextConfig = {};
+
 export default nextConfig;
