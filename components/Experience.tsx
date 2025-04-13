92bce7278e539cc19fc96b1746b58db8e44051c4
shivamk001 <shivam.kesarwani001@gmail.com>
2025-04-13 20:28:43 +0530

    client section

---
+++ components/Experience.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Experience = () => {
+    return (
+        <div className='py-20' id='testimonials'>
+            <h1 className='heading text-center font-bold text-[20px] md:text-2xl lg:text-5xl'>
+                Kind words from
+                <span className='text-purple'>satisfied clients</span>
+            </h1>
+            <div className='flex flex-col items-center max-lg:mt-10'>
+            </div>
+        </div>
+)};
+
+export default Experience
