3910933da5048405157b4f077bd96fca95c025b0
shivamk001 <shivam.kesarwani001@gmail.com>
2025-04-13 23:22:23 +0530

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,13 @@ import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavBar";
+import Footer from "@/components/ui/Footer";
 import { navItems } from "@/data";
-import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center 
+    flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav
           navItems={navItems}
@@ -19,6 +20,7 @@ export default function Home() {
         <RecentProjects/>
         <Clients/>
         <Experience/>
+        <Footer/>
       </div>
     </main>
   );
