bacbc9faa73d1cdcc51784733e371b316aafa389
shivamk001 <shivam.kesarwani001@gmail.com>
2025-04-22 23:26:03 +0530

    changed basePath, changed img path in index.ts

--- data/index.ts
+++ data/index.ts
@@ -13,7 +13,7 @@ export const gridItems = [
         className: "lg:col-span-1 md:col-span-6 md:row-span-2 lg:min-h-[60vh]",
         imgClassName: "w-full h-full",
         titleClassName: "justify-end",
-        img: "/b1.svg",
+        img: "/portfolio/b1.svg",
         spareImg: "",
     },
     // {
@@ -43,8 +43,8 @@ export const gridItems = [
         // className: "lg:col-span-2 md:col-span-3 md:row-span-1",
         imgClassName: "",
         titleClassName: "justify-start",
-        img: "/grid.svg",
-        spareImg: "/b4.svg",
+        img: "/portfolio/grid.svg",
+        spareImg: "/portfolio/b4.svg",
     },
 
     {
@@ -54,8 +54,8 @@ export const gridItems = [
         // className: "md:col-span-3 md:row-span-2",
         imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
         titleClassName: "justify-center md:justify-start lg:justify-center",
-        img: "/b5.svg",
-        spareImg: "/grid.svg",
+        img: "/portfolio/b5.svg",
+        spareImg: "/portfolio/grid.svg",
     },
     {
         id: 6,
@@ -74,32 +74,32 @@ export const projects = [
         id: 1,
         title: "GitTix - Ticketing Reselling Platform",
         des: "A Platform to buy and sell tickets.",
-        img: "/ticketResell.png",
-        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/node.svg", "/mongodb.svg"],
+        img: "/portfolio/ticketResell.png",
+        iconLists: ["/portfolio/next.svg", "/portfolio/tail.svg", "/portfolio/ts.svg", "/portfolio/node.svg", "/portfolio/mongodb.svg"],
         link: "http://shivamkesarwani.in/",
     },
     {
         id: 2,
         title: "Issue Tracker - Issue Reporting App",
         des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-        img: "/issueTracker.png",
-        iconLists: ["/node.svg", "/mongodb.svg"],
+        img: "/portfolio/issueTracker.png",
+        iconLists: ["/portfolio/node.svg", "/portfolio/mongodb.svg"],
         link: "http://shivamkesarwani.in/issuetracker",
     },
     {
         id: 3,
         title: "CSV File Reader",
         des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-        img: "/csvfile.png",
-        iconLists: ["/node.svg"],
+        img: "/portfolio/csvfile.png",
+        iconLists: ["/portfolio/node.svg"],
         link: "https://csv-upload-9ljq.onrender.com/",
     },
     {
         id: 4,
         title: "BuyBusy ECommerce Application",
         des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-        img: "/buybusy.png",
-        iconLists: ["/re.svg", "/firebase.svg"],
+        img: "/portfolio/buybusy.png",
+        iconLists: ["/portfolio/re.svg", "/portfolio/firebase.svg"],
         link: "https://busybuy-shivamk001.netlify.app/",
     },
 ];
@@ -141,32 +141,32 @@ export const companies = [
     {
         id: 1,
         name: "cloudinary",
-        img: "/cloud.svg",
-        nameImg: "/cloudName.svg",
+        img: "/portfolio/cloud.svg",
+        nameImg: "/portfolio/cloudName.svg",
     },
     {
         id: 2,
         name: "appwrite",
-        img: "/app.svg",
-        nameImg: "/appName.svg",
+        img: "/portfolio/app.svg",
+        nameImg: "/portfolio/appName.svg",
     },
     {
         id: 3,
         name: "HOSTINGER",
-        img: "/host.svg",
-        nameImg: "/hostName.svg",
+        img: "/portfolio/host.svg",
+        nameImg: "/portfolio/hostName.svg",
     },
     {
         id: 4,
         name: "stream",
-        img: "/s.svg",
-        nameImg: "/streamName.svg",
+        img: "/portfolio/s.svg",
+        nameImg: "/portfolio/streamName.svg",
     },
     {
         id: 5,
         name: "docker.",
-        img: "/dock.svg",
-        nameImg: "/dockerName.svg",
+        img: "/portfolio/dock.svg",
+        nameImg: "/portfolio/dockerName.svg",
     },
 ];
 
@@ -176,21 +176,21 @@ export const workExperience = [
         title: "System Engineer - TCS",
         desc: "Contributed in the end-to-end development of a mission critical data-intensive internal web application.",
         className: "md:col-span-2",
-        thumbnail: "/exp1.svg",
+        thumbnail: "/portfolio/exp1.svg",
     },
     {
         id: 2,
         title: "Backend Developer - Growlytics",
         desc: "Contributed in the feature development of Growlytics Application.",
         className: "md:col-span-2", // change to md:col-span-2
-        thumbnail: "/exp2.svg",
+        thumbnail: "/portfolio/exp2.svg",
     },
     {
         id: 3,
         title: "Software Engineer - Kenscio",
         desc: "Designed and developed Kenlyser to monitor email volumes. Contributed to the feature development of Growlytics Application.",
         className: "md:col-span-2", // change to md:col-span-2
-        thumbnail: "/exp4.svg",
+        thumbnail: "/portfolio/exp4.svg",
     },
     // {
     //     id: 4,
@@ -204,14 +204,14 @@ export const workExperience = [
 export const socialMedia = [
     {
         id: 1,
-        img: "/git.svg",
+        img: "/portfolio/git.svg",
     },
     {
         id: 2,
-        img: "/twit.svg",
+        img: "/portfolio/twit.svg",
     },
     {
         id: 3,
-        img: "/link.svg",
+        img: "/portfolio/link.svg",
     },
 ];
