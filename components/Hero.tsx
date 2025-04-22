b2a13bafb83f8cf2106692a3e020190429602d6d
shivamk001 <shivam.kesarwani001@gmail.com>
2025-04-22 22:49:55 +0530

    removed Dynamic Web Magic with Next.js

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -35,9 +35,9 @@ const Hero = () => {
 
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
+          {/* <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
             Dynamic Web Magic with Next.js
-          </h2>
+          </h2> */}
           <TextGenerateEffect
             words="Turning Complex Logic into Scalable, Reliable Systems"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
